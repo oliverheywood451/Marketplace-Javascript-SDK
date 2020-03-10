14f0aa42268b65cbc984b11016347b4bbe193bec
Oliver Heywood <oheywood@four51.com>
2020-03-10 13:52:50 -0500

    1.1.5

--- codegen/templates/models/ListArgs.ts
+++ codegen/templates/models/ListArgs.ts
@@ -2,8 +2,8 @@ import { Filters } from './Filters'
 
 export interface ListArgs<T = any> {
   search?: string
-  searchOn?: string[]
-  sortBy?: string[]
+  searchOn?: string
+  sortBy?: string
   page?: number
   pageSize?: number
   filters?: Filters<Required<T>>
