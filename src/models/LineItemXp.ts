27c10dd2c897ecc41fdd2d354a3d6c08442e0b28
Oliver Heywood <oheywood@four51.com>
2020-06-02 11:47:24 -0500

    v 1.4.3 adding LineItemImageUrl

--- src/models/LineItemXp.ts
+++ src/models/LineItemXp.ts
@@ -2,5 +2,6 @@ import { LineItemReturnInfo } from './LineItemReturnInfo';
 
 export interface LineItemXp {
     LineItemReturnInfo?: LineItemReturnInfo
+    LineItemImageUrl?: string
     ProductUnitPrice?: number
 }
