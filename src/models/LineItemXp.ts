3478a4f877bc87dd7c7ea922c810c90e68c23a86
Oliver Heywood <oheywood@four51.com>
2020-06-16 15:23:26 -0500

    v1.4.6 add order and lineItem statuses

--- src/models/LineItemXp.ts
+++ src/models/LineItemXp.ts
@@ -1,6 +1,7 @@
 import { LineItemReturnInfo } from './LineItemReturnInfo';
 
 export interface LineItemXp {
+    LineItemStatus?: 'Complete' | 'Submitted' | 'Open' | 'Backordered' | 'Cancelled' | 'Returned'
     LineItemReturnInfo?: LineItemReturnInfo
     LineItemImageUrl?: string
     UnitPriceInProductCurrency?: number
--- src/models/OrderXp.ts
+++ src/models/OrderXp.ts
@@ -11,4 +11,7 @@ export interface OrderXp {
     QuoteOrderInfo?: QuoteOrderInfo
     OrderReturnInfo?: OrderReturnInfo
     Currency?: 'CAD' | 'HKD' | 'ISK' | 'PHP' | 'DKK' | 'HUF' | 'CZK' | 'GBP' | 'RON' | 'SEK' | 'IDR' | 'INR' | 'BRL' | 'RUB' | 'HRK' | 'JPY' | 'THB' | 'CHF' | 'EUR' | 'MYR' | 'BGN' | 'TRY' | 'CNY' | 'NOK' | 'NZD' | 'ZAR' | 'USD' | 'MXN' | 'SGD' | 'AUD' | 'ILS' | 'KRW' | 'PLN'
+    ShippingStatus?: 'Shipped' | 'PartiallyShipped' | 'Cancelled' | 'Processing'
+    LineItemStatus?: 'Complete' | 'Submitted' | 'Open' | 'Backordered' | 'Cancelled' | 'Returned'
+    ClaimStatus?: 'NoClaim' | 'Pending' | 'Complete'
 }
