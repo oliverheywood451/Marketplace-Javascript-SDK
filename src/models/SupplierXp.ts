2dd717f74066679a840921f437d0cb167a377ced
Oliver Heywood <oheywood@four51.com>
2020-06-17 12:46:22 -0500

    v1.4.7 bringing it all together.

--- src/models/SupplierXp.ts
+++ src/models/SupplierXp.ts
@@ -1,9 +1,7 @@
-import { ProductImage } from './ProductImage';
 import { Contact } from './Contact';
 
 export interface SupplierXp {
     Description?: string
-    Images?: ProductImage[]
     SupportContact?: Contact
     SyncFreightPop?: boolean
     ApiClientID?: string
